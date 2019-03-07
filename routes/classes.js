c9a340c6b80de92f370497e3b6c96773cb9e8611
ruiaraujo012 <ruiaraujo012@gmail.com>
2019-03-07 12:33:19 +0000

    Revert "Documentacao inicial classes e entidades. Remocao de metodos nao necessarios na gestao do user"
    This reverts commit d623e16fcf04b9938411c261db07899d826d9e69.

--- routes/classes.js
+++ routes/classes.js
@@ -6,21 +6,16 @@ const authenticate = require('../auth/auth').authenticate
 
 /**
  * @swagger
- * /classes/{nivel}:
+ * /classes:
  *   get:
  *     tags:
- *       - Obter lista de classes por nível
- *     description: Devolve um array de classes, cada um composto por código, titulo e id.
- *     parameters:
- *       - in: path
- *         name: nivel
+ *       - Get all classes Page
+ *     description: Returns index page
  *     produces:
  *       - application/json
  *     responses:
  *       200:
- *         description:
- *         examples:
- *           application/json: [{"codigo":{"type":"literal","value":"100"},"titulo":{"type":"literal","value":"ORDENAMENTO JURÍDICO E NORMATIVO"},"id":{"type":"uri","value":"http://jcr.di.uminho.pt/m51-clav#c100"}},{"codigo":{"type":"literal","value":"150"},"titulo":{"type":"literal","value":"PLANEAMENTO E GESTÃO ESTRATÉGICA"},"id":{"type":"uri","value":"http://jcr.di.uminho.pt/m51-clav#c150"}}]
+ *         description: Get all classes page
  */
 
 router.get('/:nivel', authenticate(), (req, res) => {
